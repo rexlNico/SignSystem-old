ac50332c5a37256bd5d3e8be901bca469bff9305
rexlNico <rexlNico@users.noreply.github.com>
2021-11-05 04:02:00 +0100

    Commit

---
+++ src/de/rexlNico/Signs/Api/Config.java
@@ -0,0 +1,13 @@
+package de.rexlNico.Signs.Api;
+
+import java.io.File;
+
+public class Config {
+
+	public File file = new File("plugins/SignSystem/config.yml");
+	
+	public Config() {
+		
+	}
+	
+}
